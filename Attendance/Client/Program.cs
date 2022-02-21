6383133c9f2cd4da1ee75f838de81003cb4b36a0
Mohammad Komaei <komaei@live.com>
2022-02-21 15:14:43 +0330

    remove AddPolicies()

--- Attendance/Client/Program.cs
+++ Attendance/Client/Program.cs
@@ -12,7 +12,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddPolicies();
+builder.Services.AddAuthorizationCore();
+//builder.Services.AddPolicies();
 builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
 
 await builder.Build().RunAsync();
