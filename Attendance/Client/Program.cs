3d30c745536d861b16bc895e8ed911a5fa0a852e
MK <komaei@live.com>
2022-03-30 16:56:49 +0430

    log level

--- Attendance/Client/Program.cs
+++ Attendance/Client/Program.cs
@@ -8,12 +8,10 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddAuthorizationCore();
 //builder.Services.AddPolicies();
 builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
-
+builder.Logging.SetMinimumLevel(LogLevel.Warning);
 await builder.Build().RunAsync();
