a8408165e8c845bdb75cbb2c472004fdb611fac2
Mohammad Komaei <komaei@live.com>
2022-05-21 17:23:56 +0430

    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles

--- Attendance/Server/Program.cs
+++ Attendance/Server/Program.cs
@@ -1,4 +1,5 @@
 using Attendance.Server.Configs;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,7 +8,12 @@ builder.Services.AddDataBase(builder.Configuration);
 builder.Services.AddIdentityAndOptions();
 builder.Services.AddJwt(builder.Configuration);
 builder.Services.AddControllers()
-    .AddJsonOptions(op => { op.JsonSerializerOptions.PropertyNamingPolicy = null; }).AddBadRequestServices();
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = null;
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    })
+    .AddBadRequestServices();
 //builder.Services.AddRazorPages();
 
 var app = builder.Build();
